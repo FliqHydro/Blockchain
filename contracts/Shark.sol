9bdf85d77a110a41acb481e9acbac2b360aee052
FliqHydro <charles.whiting1@gmail.com>
2018-03-07 10:21:20 -0800

    Check for errors for SHarkRace.sol

--- contracts/Shark.sol
+++ contracts/Shark.sol
@@ -2,25 +2,37 @@ pragma solidity ^0.4.18;
 
 contract Shark {
     string[10] public species;
-    address[] public owners;
-    mapping (address => SharkObject) owners
+    SharkObject[] public sharks;
+    mapping (uint => address) owners;
+    mapping (address=> uint) ownerSharkCount;
     struct SharkObject {
         uint speed;
         uint strength;
-        string type;
+        string typeOfShark;
     }
 
     function createBasicShark() public {
-        owners[msg.sender] = SharkObject(10, 10, "black tip reef shark");
+        uint id = sharks.push(SharkObject(10, 10, "blacktip reef shark")) - 1;
+        owners[id] = msg.sender;
+        ownerSharkCount[msg.sender]++;
     } 
 
-    function trainShark() public {
-        owners[msg.sender]; 
+    function trainShark(uint _sharkId) public {
+        require(msg.sender == owners[_sharkId]);
+        sharks[_sharkId].speed ++;
+        sharks[_sharkId].strength ++; 
         // TODO: Increase Speed or Strength
     }  
     //TODO: Double check struct syntax
-    function getShark() public view returns (struct) {
-        return owners[msg.sender];
-
+    function getShark() public view returns (uint[]) {
+        uint[] memory result = new uint[](ownerSharkCount[msg.sender]);
+        uint counter = 0;
+        for (uint i = 0; i < sharks.length; i++) {
+            if (owners[i] == msg.sender) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
     }
 }
