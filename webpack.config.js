0891fc88fd1b6fa021fde240d2949dc1d951f144
FliqHydro <charles.whiting1@gmail.com>
2018-03-24 13:23:07 -0700

    create empty pages for website

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
   plugins: [
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
-      { from: './app/index.html', to: "index.html" }
+      { from: './app/index.html', to: "index.html" },
+      { from: './app/auction.html', to: "auction.html" },
+      { from: './app/race.html', to: "race.html" },
+      { from: './app/settings.html', to: "settings.html" },
+      { from: './app/training.html', to: "training.html" }
     ])
   ],
   module: {
