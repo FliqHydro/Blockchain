fa458e9f4aabc61535dcac83d6a6d3305c5d7dfa
Raymond Sapida <ray.sapida@gmail.com>
2018-05-12 08:34:47 -0700

    Create a basic shark from your account; link frontend with contract

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,15 +1,15 @@
-// Import the page's CSS. Webpack will know what to do with it.
 import "../stylesheets/app.css";
 
-// Import libraries we need.
 import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
-// Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+import shark_artifacts from '../../build/contracts/Shark.json'
+import shark_race_artifacts from '../../build/contracts/SharkRace.json'
+import shark_ownership_artifacts from '../../build/contracts/SharkOwnership.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
+var Shark = contract(shark_artifacts);
+var SharkRace = contract(shark_race_artifacts);
+var SharkOwnership = contract(shark_ownership_artifacts);
 
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
@@ -21,8 +21,9 @@ window.App = {
   start: function() {
     var self = this;
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider);
+    Shark.setProvider(web3.currentProvider);
+    SharkRace.setProvider(web3.currentProvider);
+    SharkOwnership.setProvider(web3.currentProvider);
 
     // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function(err, accs) {
@@ -39,51 +40,35 @@ window.App = {
       accounts = accs;
       account = accounts[0];
 
-      self.refreshBalance();
+      self.setEventHandlers();
     });
   },
 
-  setStatus: function(message) {
-    var status = document.getElementById("status");
-    status.innerHTML = message;
-  },
-
-  refreshBalance: function() {
+  setEventHandlers: function() {
     var self = this;
 
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
-    }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error getting balance; see log.");
-    });
+    // Creating a basic shark from the index page
+    $("#basicShark").click(function(e) {
+      var shark
+      console.dir(Shark)
+      Shark.deployed().then((instance) => {
+        shark = instance;
+        console.log('Shark instance');
+        console.dir(shark);
+
+        return shark.createBasicShark({from: account})
+        console.log('creating basic shark')
+      }).then((data) => {
+        console.dir(data)
+        alert('Shark created');
+      }).catch((e) => {
+        console.error(e)
+        alert('There was an error');
+      })
+    })
+
   },
 
-  sendCoin: function() {
-    var self = this;
-
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
-    }).then(function() {
-      self.setStatus("Transaction complete!");
-      self.refreshBalance();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error sending coin; see log.");
-    });
-  }
 };
 
 window.addEventListener('load', function() {
