1b7b9178d7b939ae6bd8f827b7d87299d3fefba1
Raymond Sapida <ray.sapida@gmail.com>
2018-05-12 03:11:06 -0700

    Update getShark to be callable by any address
    - Add tests for the SharkRace contract

--- test/TestMetacoin.sol
+++ test/TestMetacoin.sol
@@ -2,24 +2,49 @@ pragma solidity ^0.4.2;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/MetaCoin.sol";
+import "../contracts/Shark.sol";
 
-contract TestMetacoin {
+// contract TestMetacoin {
+
+//   function testInitialBalanceUsingDeployedContract() public {
+//     MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
+
+//     uint expected = 10000;
+
+//     Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+//   }
+
+//   function testInitialBalanceWithNewMetaCoin() public {
+//     MetaCoin meta = new MetaCoin();
+
+//     uint expected = 10000;
+
+//     Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+//   }
+
+// }
+
+contract TestShark {
+  // function beforeEach() {
+  //   someValue = 5;
+  // }
 
   function testInitialBalanceUsingDeployedContract() public {
-    MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
+    Shark shark = Shark(DeployedAddresses.Shark());
+    shark.createBasicShark();
 
     uint expected = 10000;
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+    // Assert.equal(shark.getBalance(tx.origin), expected, "Owner should have 10000 Shark initially");
+    // Assert.equal(shark.species(), expected, "Owner should have 10000 Shark initially");
   }
 
-  function testInitialBalanceWithNewMetaCoin() public {
-    MetaCoin meta = new MetaCoin();
+  // function testInitialBalanceWithNewMetaCoin() public {
+  //   Shark shark = new Shark();
 
-    uint expected = 10000;
+  //   uint expected = 10000;
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
-  }
+  //   Assert.equal(shark.getBalance(tx.origin), expected, "Owner should have 10000 Shark initially");
+  // }
 
 }
--- test/shark.js
+++ test/shark.js
@@ -1,4 +1,5 @@
 var Shark = artifacts.require("./Shark.sol");
+var SharkRace = artifacts.require("./SharkRace.sol");
 
 contract('Shark', function(accounts) {
   it("access the shark owners", function() {
@@ -80,4 +81,89 @@ contract('Shark', function(accounts) {
 
 
   });
+});
+
+contract('SharkRace', function(accounts) {
+
+  it("able to  race shark", function() {
+    var account_one = accounts[0];
+    var account_two = accounts[1];
+    var shark;
+    var sharkId;
+    var enemySharkId;
+    var enemyShark;
+
+    return Shark.deployed().then((instance) => {
+      shark = instance;
+      return instance.getSharks.call();
+    })
+      .then((balance) => {
+        assert.equal(balance.length, 0, "No owners initially");
+        return shark.createBasicShark();
+      })
+      .then((block) => {
+
+        return shark.getSharks.call();
+
+      })
+      .then((sharkArray) => {
+        sharkId = sharkArray[0].toNumber();
+
+        assert.equal(sharkArray.length, 1, "Add one shark after calling createBasicShark");
+        return shark.getShark.call(sharkId);
+      })
+      .then((data) => {
+        let speed = 10;
+        let strength = 10;
+        let type = 'blacktip reef shark';
+        let winCount = 0;
+        let level = 1;
+        let lossCount = 0;
+
+        assert.equal(data[0], speed, "The speed of the shark is 10");
+        assert.equal(data[1], strength, "The strength of the shark is 10");
+        assert.equal(data[2], type, "The type of shark is a: " + type);
+        assert.equal(data[3], winCount, "The win count of the shark is 0");
+        assert.equal(data[4], level, "The level of the shark is 1");
+        assert.equal(data[5], lossCount, "The loss count of the shark is 0");
+
+        return shark.createBasicShark( {from: account_two})
+      })
+      .then((block) => {
+
+        return shark.getSharks.call( {from: account_two} );
+
+      })
+      .then((sharkArray) => {
+        enemySharkId = sharkArray[0].toNumber();
+
+        assert.equal(sharkArray.length, 1, "Add one shark after calling createBasicShark");
+        return shark.getShark.call(enemySharkId);
+      })
+      .then((data) => {
+        let speed = 10;
+        let strength = 10;
+        let type = 'blacktip reef shark';
+        let winCount = 0;
+        let level = 1;
+        let lossCount = 0;
+
+        assert.equal(data[0], speed, "The speed of the shark is 10");
+        assert.equal(data[1], strength, "The strength of the shark is 10");
+        assert.equal(data[2], type, "The type of shark is a: " + type);
+        assert.equal(data[3], winCount, "The win count of the shark is 0");
+        assert.equal(data[4], level, "The level of the shark is 1");
+        assert.equal(data[5], lossCount, "The loss count of the shark is 0");
+
+        return SharkRace.deployed().then((instance) => {
+          instance.sharkRace(sharkId, enemySharkId)
+        })
+          .then((data) => {
+            console.log(data)
+          })
+
+      })
+
+  });
+
 });
