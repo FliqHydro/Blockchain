811f236ab72f5de80a9d7308308d83653f441fc8
Raymond Sapida <ray.sapida@gmail.com>
2018-05-12 03:30:45 -0700

    Update migrations to migrate SharkRace and SharkOwnership contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,17 @@
-// var ConvertLib = artifacts.require("./ConvertLib.sol");
 var Shark = artifacts.require("./Shark.sol");
+var SharkRace = artifacts.require("./SharkRace.sol");
+
+// var ERC721 = artifacts.require("./erc721.sol");
+// var SafeMath = artifacts.require("./SafeMath.sol");
+
+var SharkOwnership = artifacts.require("./SharkOwnership.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Shark);
+  deployer.deploy(SharkRace);
+
+  // deployer.deploy(ERC721);
+  // deployer.deploy(SafeMath);
+
+  deployer.deploy(SharkOwnership);
 };
